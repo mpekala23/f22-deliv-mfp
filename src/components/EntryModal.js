c5d268960375b4e55ae98399d239a14ffe5e47ed
Mark Pekala <mpekala@college.harvard.edu>
2022-09-12 00:21:57 -0400

    required stages complete

--- src/components/EntryModal.js
+++ src/components/EntryModal.js
@@ -1,19 +1,20 @@
-import OpenInNewIcon from '@mui/icons-material/OpenInNew';
-import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import FormControl from '@mui/material/FormControl';
-import IconButton from '@mui/material/IconButton';
-import InputLabel from '@mui/material/InputLabel';
-import MenuItem from '@mui/material/MenuItem';
-import Select from '@mui/material/Select';
-import TextField from '@mui/material/TextField';
-import * as React from 'react';
-import { useState } from 'react';
-import { categories } from '../utils/categories';
-import { addEntry } from '../utils/mutations';
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import { DialogContentText } from "@mui/material";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogTitle from "@mui/material/DialogTitle";
+import FormControl from "@mui/material/FormControl";
+import IconButton from "@mui/material/IconButton";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import * as React from "react";
+import { useState } from "react";
+import { categories } from "../utils/categories";
+import { addEntry, deleteEntry, updateEntry } from "../utils/mutations";
 
 // Modal component for individual entries.
 
@@ -25,129 +26,178 @@ type: Type of entry modal being opened.
 user: User making query (The current logged in user). */
 
 export default function EntryModal({ entry, type, user }) {
+  // State variables for modal status
 
-   // State variables for modal status
+  const [open, setOpen] = useState(false);
+  const [name, setName] = useState(entry.name);
+  const [link, setLink] = useState(entry.link);
+  const [description, setDescription] = useState(entry.description);
+  const [category, setCategory] = useState(entry.category);
+  const [deleteConfirmOpen, setDeleteConfirmOpen] = useState(false);
 
-   // TODO: For editing, you may have to add and manage another state variable to check if the entry is being edited.
+  // Modal visibility handlers
 
-   const [open, setOpen] = useState(false);
-   const [name, setName] = useState(entry.name);
-   const [link, setLink] = useState(entry.link);
-   const [description, setDescription] = useState(entry.description);
-   const [category, setCategory] = React.useState(entry.category);
+  const handleClickOpen = () => {
+    setOpen(true);
+    setName(entry.name);
+    setLink(entry.link);
+    setDescription(entry.description);
+    setCategory(entry.category);
+  };
 
-   // Modal visibility handlers
+  const handleClose = () => {
+    setOpen(false);
+  };
 
-   const handleClickOpen = () => {
-      setOpen(true);
-      setName(entry.name);
-      setLink(entry.link);
-      setDescription(entry.description);
-      setCategory(entry.category);
-   };
+  const handleDeleteClose = () => {
+    setDeleteConfirmOpen(false);
+  };
 
-   const handleClose = () => {
-      setOpen(false);
-   };
+  // Mutation handlers
 
-   // Mutation handlers
+  const handleAdd = () => {
+    const newEntry = {
+      name: name,
+      link: link,
+      description: description,
+      user: user?.displayName ? user?.displayName : "GenericUser",
+      category: category,
+      userid: user?.uid,
+    };
 
-   const handleAdd = () => {
-      const newEntry = {
-         name: name,
-         link: link,
-         description: description,
-         user: user?.displayName ? user?.displayName : "GenericUser",
-         category: category,
-         userid: user?.uid,
-      };
+    addEntry(newEntry).catch(console.error);
+    handleClose();
+  };
 
-      addEntry(newEntry).catch(console.error);
-      handleClose();
-   };
+  const handleUpdate = () => {
+    const updatedEntry = {
+      name: name,
+      link: link,
+      description: description,
+      user: user?.displayName || "GenericUser",
+      category: category,
+      userid: user?.uid,
+      id: entry.id,
+    };
 
-   // TODO: Add Edit Mutation Handler
+    updateEntry(updatedEntry).catch(console.error);
+    handleClose();
+  };
 
-   // TODO: Add Delete Mutation Handler
+  const handleDelete = () => {
+    deleteEntry(entry.id);
+    handleDeleteClose();
+  };
 
-   // Button handlers for modal opening and inside-modal actions.
-   // These buttons are displayed conditionally based on if adding or editing/opening.
-   // TODO: You may have to edit these buttons to implement editing/deleting functionality.
+  // TODO: Add Delete Mutation Handler
 
-   const openButton =
-      type === "edit" ? <IconButton onClick={handleClickOpen}>
-         <OpenInNewIcon />
+  // Button handlers for modal opening and inside-modal actions.
+  // These buttons are displayed conditionally based on if adding or editing/opening.
+  // TODO: You may have to edit these buttons to implement editing/deleting functionality.
+
+  const openButton =
+    type === "edit" ? (
+      <IconButton onClick={handleClickOpen}>
+        <OpenInNewIcon />
       </IconButton>
-         : type === "add" ? <Button variant="contained" onClick={handleClickOpen}>
-            Add entry
-         </Button>
-            : null;
+    ) : type === "add" ? (
+      <Button variant="contained" onClick={handleClickOpen}>
+        Add entry
+      </Button>
+    ) : null;
 
-   const actionButtons =
-      type === "edit" ?
-         <DialogActions>
-            <Button onClick={handleClose}>Cancel</Button>
-         </DialogActions>
-         : type === "add" ?
-            <DialogActions>
-               <Button onClick={handleClose}>Cancel</Button>
-               <Button variant="contained" onClick={handleAdd}>Add Entry</Button>
-            </DialogActions>
-            : null;
+  const actionButtons =
+    type === "edit" ? (
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button variant="contained" onClick={handleUpdate}>
+          Update Entry
+        </Button>
+        <Button
+          color="warning"
+          variant="outlined"
+          onClick={() => setDeleteConfirmOpen(true)}
+        >
+          Delete Entry
+        </Button>
+        <Dialog open={deleteConfirmOpen}>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              Are you sure you want to delete this entry?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={handleDeleteClose}>No</Button>
+            <Button onClick={handleDelete} autoFocus>
+              Delete
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </DialogActions>
+    ) : type === "add" ? (
+      <DialogActions>
+        <Button onClick={handleClose}>Cancel</Button>
+        <Button variant="contained" onClick={handleAdd}>
+          Add Entry
+        </Button>
+      </DialogActions>
+    ) : null;
 
-   return (
-      <div>
-         {openButton}
-         <Dialog open={open} onClose={handleClose}>
-            <DialogTitle>{type === "edit" ? name : "Add Entry"}</DialogTitle>
-            <DialogContent>
-               {/* TODO: Feel free to change the properties of these components to implement editing functionality. The InputProps props class for these MUI components allows you to change their traditional CSS properties. */}
-               <TextField
-                  margin="normal"
-                  id="name"
-                  label="Name"
-                  fullWidth
-                  variant="standard"
-                  value={name}
-                  onChange={(event) => setName(event.target.value)}
-               />
-               <TextField
-                  margin="normal"
-                  id="link"
-                  label="Link"
-                  placeholder="e.g. https://google.com"
-                  fullWidth
-                  variant="standard"
-                  value={link}
-                  onChange={(event) => setLink(event.target.value)}
-               />
-               <TextField
-                  margin="normal"
-                  id="description"
-                  label="Description"
-                  fullWidth
-                  variant="standard"
-                  multiline
-                  maxRows={8}
-                  value={description}
-                  onChange={(event) => setDescription(event.target.value)}
-               />
+  return (
+    <div>
+      {openButton}
+      <Dialog open={open} onClose={handleClose}>
+        <DialogTitle>{type === "edit" ? name : "Add Entry"}</DialogTitle>
+        <DialogContent>
+          {/* TODO: Feel free to change the properties of these components to implement editing functionality. The InputProps props class for these MUI components allows you to change their traditional CSS properties. */}
+          <TextField
+            margin="normal"
+            id="name"
+            label="Name"
+            fullWidth
+            variant="standard"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <TextField
+            margin="normal"
+            id="link"
+            label="Link"
+            placeholder="e.g. https://google.com"
+            fullWidth
+            variant="standard"
+            value={link}
+            onChange={(event) => setLink(event.target.value)}
+          />
+          <TextField
+            margin="normal"
+            id="description"
+            label="Description"
+            fullWidth
+            variant="standard"
+            multiline
+            maxRows={8}
+            value={description}
+            onChange={(event) => setDescription(event.target.value)}
+          />
 
-               <FormControl fullWidth sx={{ "margin-top": 20 }}>
-                  <InputLabel id="demo-simple-select-label">Category</InputLabel>
-                  <Select
-                     labelId="demo-simple-select-label"
-                     id="demo-simple-select"
-                     value={category}
-                     label="Category"
-                     onChange={(event) => setCategory(event.target.value)}
-                  >
-                     {categories.map((category) => (<MenuItem value={category.id}>{category.name}</MenuItem>))}
-                  </Select>
-               </FormControl>
-            </DialogContent>
-            {actionButtons}
-         </Dialog>
-      </div>
-   );
+          <FormControl fullWidth sx={{ "margin-top": 20 }}>
+            <InputLabel id="demo-simple-select-label">Category</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={category}
+              label="Category"
+              onChange={(event) => setCategory(event.target.value)}
+            >
+              {categories.map((category) => (
+                <MenuItem value={category.id}>{category.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </DialogContent>
+        {actionButtons}
+      </Dialog>
+    </div>
+  );
 }
--- src/utils/mutations.js
+++ src/utils/mutations.js
@@ -1,33 +1,46 @@
-import { addDoc, collection } from "firebase/firestore";
-import { db } from './firebase';
+import {
+  addDoc,
+  doc,
+  updateDoc,
+  collection,
+  deleteDoc,
+} from "firebase/firestore";
+import { db } from "./firebase";
 
 // Functions for database mutations
 
 export const emptyEntry = {
-   name: "",
-   link: "",
-   description: "",
-   user: "",
-   category: 0,
-}
+  name: "",
+  link: "",
+  description: "",
+  user: "",
+  category: 0,
+};
 
 export async function addEntry(entry) {
-   await addDoc(collection(db, "entries"), {
-      name: entry.name,
-      link: entry.link,
-      description: entry.description,
-      user: entry.user,
-      category: entry.category,
-      // The ID of the current user is logged with the new entry for database user-access functionality.
-      // You should not remove this userid property, otherwise your logged entries will not display.
-      userid: entry.userid,
-   });
+  await addDoc(collection(db, "entries"), {
+    name: entry.name,
+    link: entry.link,
+    description: entry.description,
+    user: entry.user,
+    category: entry.category,
+    // The ID of the current user is logged with the new entry for database user-access functionality.
+    // You should not remove this userid property, otherwise your logged entries will not display.
+    userid: entry.userid,
+  });
 }
 
 export async function updateEntry(entry) {
-   // TODO: Create Mutation to Edit Entry
+  const docRef = doc(db, "entries", entry.id);
+  await updateDoc(docRef, {
+    name: entry.name,
+    link: entry.link,
+    description: entry.description,
+    category: entry.category,
+  });
 }
 
-export async function deleteEntry(entry) {
-   // TODO: Create Mutation to Delete Entry
+export async function deleteEntry(entryId) {
+  const docRef = doc(db, "entries", entryId);
+  await deleteDoc(docRef);
 }
